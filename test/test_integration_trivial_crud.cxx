ef66a562220e81285cfbbe3d33412031c73df2e2
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2021-11-12 22:01:58 +0000

    Port mutation integration tests from lcb

--- test/test_integration_trivial_crud.cxx
+++ test/test_integration_trivial_crud.cxx
@@ -23,21 +23,75 @@ static const tao::json::value basic_doc = {
 };
 static const std::string basic_doc_json = couchbase::utils::json::generate(basic_doc);
 
-TEST_CASE("integration: upsert document into default collection", "[integration]")
+TEST_CASE("integration: crud on default collection", "[integration]")
 {
     test::utils::integration_test_guard integration;
 
     test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+    couchbase::document_id id{ integration.ctx.bucket, "_default", "_default", test::utils::uniq_id("foo") };
 
+    // create
     {
-        couchbase::document_id id{ integration.ctx.bucket, "_default", "_default", test::utils::uniq_id("foo") };
-        couchbase::operations::upsert_request req{ id, basic_doc_json };
+        couchbase::operations::insert_request req{ id, basic_doc_json };
         auto resp = test::utils::execute(integration.cluster, req);
         INFO(resp.ctx.ec.message())
         REQUIRE_FALSE(resp.ctx.ec);
         REQUIRE(!resp.cas.empty());
         INFO("seqno=" << resp.token.sequence_number)
         REQUIRE(resp.token.sequence_number != 0);
+    }
+
+    // read
+    {
+        couchbase::operations::get_request req{ id };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(resp.value == basic_doc_json);
+    }
+
+    // update
+    {
+        auto doc = basic_doc;
+        auto json = couchbase::utils::json::generate(doc);
+        doc["a"] = 2.0;
+
+        {
+            couchbase::operations::replace_request req{ id, json };
+            auto resp = test::utils::execute(integration.cluster, req);
+            REQUIRE_FALSE(resp.ctx.ec);
+        }
+
+        {
+            couchbase::operations::get_request req{ id };
+            auto resp = test::utils::execute(integration.cluster, req);
+            REQUIRE(resp.value == json);
+        }
+
+        {
+            couchbase::operations::upsert_request req{ id, basic_doc_json };
+            auto resp = test::utils::execute(integration.cluster, req);
+            REQUIRE_FALSE(resp.ctx.ec);
+        }
+
+        {
+            couchbase::operations::get_request req{ id };
+            auto resp = test::utils::execute(integration.cluster, req);
+            REQUIRE(resp.value == basic_doc_json);
+        }
+    }
+
+    // delete
+    {
+        {
+            couchbase::operations::remove_request req{ id };
+            auto resp = test::utils::execute(integration.cluster, req);
+            REQUIRE_FALSE(resp.ctx.ec);
+        }
+
+        {
+            couchbase::operations::get_request req{ id };
+            auto resp = test::utils::execute(integration.cluster, req);
+            REQUIRE(resp.ctx.ec == couchbase::error::key_value_errc::document_not_found);
+        }
     }
 }
 
@@ -182,6 +236,36 @@ TEST_CASE("integration: pessimistic locking", "[integration]")
     }
 }
 
+TEST_CASE("integration: lock/unlock without lock time", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+    test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+
+    couchbase::document_id id{ integration.ctx.bucket, "_default", "_default", test::utils::uniq_id("locking") };
+
+    {
+        couchbase::operations::insert_request req{ id, basic_doc_json };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+    }
+
+    couchbase::protocol::cas cas;
+
+    {
+        couchbase::operations::get_and_lock_request req{ id };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        cas = resp.cas;
+    }
+
+    {
+        couchbase::operations::unlock_request req{ id };
+        req.cas = cas;
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+    }
+}
+
 TEST_CASE("integration: touch with zero expiry resets expiry", "[integration]")
 {
     test::utils::integration_test_guard integration;
@@ -231,6 +315,13 @@ TEST_CASE("integration: exists", "[integration]")
     couchbase::document_id id{ integration.ctx.bucket, "_default", "_default", test::utils::uniq_id("exists") };
 
     {
+        couchbase::operations::exists_request req{ id };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        REQUIRE(resp.status == couchbase::operations::exists_response::observe_status::not_found);
+    }
+
+    {
         couchbase::operations::insert_request req{ id, basic_doc_json };
         auto resp = test::utils::execute(integration.cluster, req);
         REQUIRE_FALSE(resp.ctx.ec);
@@ -243,5 +334,158 @@ TEST_CASE("integration: exists", "[integration]")
         REQUIRE(resp.exists());
         REQUIRE((resp.status == couchbase::operations::exists_response::observe_status::found ||
                  resp.status == couchbase::operations::exists_response::observe_status::persisted));
+    }
+
+    {
+        couchbase::operations::remove_request req{ id };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+    }
+
+    {
+        couchbase::operations::exists_request req{ id };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        REQUIRE_FALSE(resp.exists());
+        REQUIRE((resp.status == couchbase::operations::exists_response::observe_status::logically_deleted ||
+                 resp.status == couchbase::operations::exists_response::observe_status::not_found));
+    }
+}
+
+TEST_CASE("integration: zero length value", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+    test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+
+    couchbase::document_id id{ integration.ctx.bucket, "_default", "_default", test::utils::uniq_id("zero_length_value") };
+
+    {
+        couchbase::operations::insert_request req{ id, "" };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+    }
+
+    {
+        couchbase::operations::get_request req{ id };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        REQUIRE(resp.value == "");
+    }
+}
+
+TEST_CASE("integration: ops on missing document", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+    test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+
+    couchbase::document_id id{ integration.ctx.bucket, "_default", "_default", "missing_key" };
+
+    SECTION("get")
+    {
+        couchbase::operations::get_request req{ id };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(resp.ctx.ec == couchbase::error::key_value_errc::document_not_found);
+    }
+
+    SECTION("remove")
+    {
+        couchbase::operations::remove_request req{ id };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(resp.ctx.ec == couchbase::error::key_value_errc::document_not_found);
+    }
+
+    SECTION("replace")
+    {
+        couchbase::operations::replace_request req{ id, "" };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(resp.ctx.ec == couchbase::error::key_value_errc::document_not_found);
+    }
+}
+
+TEST_CASE("integration: cas replace", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+    test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+
+    couchbase::document_id id{ integration.ctx.bucket, "_default", "_default", test::utils::uniq_id("cas_replace") };
+    couchbase::protocol::cas cas;
+
+    {
+        couchbase::operations::insert_request req{ id, basic_doc_json };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        cas = resp.cas;
+    }
+
+    SECTION("incorrect")
+    {
+        couchbase::operations::replace_request req{ id, "" };
+        req.cas = couchbase::protocol::cas{ cas.value + 1 };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(resp.ctx.ec == couchbase::error::common_errc::cas_mismatch);
+    }
+
+    SECTION("correct")
+    {
+        couchbase::operations::replace_request req{ id, "" };
+        req.cas = cas;
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+    }
+}
+
+TEST_CASE("integration: upsert preserve expiry", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+
+    if (!integration.ctx.version.supports_preserve_expiry()) {
+        return;
+    }
+
+    test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+
+    couchbase::document_id id{ integration.ctx.bucket, "_default", "_default", test::utils::uniq_id("preserve_expiry") };
+    uint32_t expiry = std::numeric_limits<uint32_t>::max();
+    auto expiry_path = "$document.exptime";
+
+    {
+        couchbase::operations::upsert_request req{ id, basic_doc_json };
+        req.expiry = expiry;
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+    }
+
+    {
+        couchbase::operations::lookup_in_request req{ id };
+        req.specs.add_spec(couchbase::protocol::subdoc_opcode::get, true, expiry_path);
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(expiry == std::stoul(resp.fields[0].value));
+    }
+
+    {
+        couchbase::operations::upsert_request req{ id, basic_doc_json };
+        req.preserve_expiry = true;
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+    }
+
+    {
+        couchbase::operations::lookup_in_request req{ id };
+        req.specs.add_spec(couchbase::protocol::subdoc_opcode::get, true, expiry_path);
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(expiry == std::stoul(resp.fields[0].value));
+    }
+
+    {
+        couchbase::operations::upsert_request req{ id, basic_doc_json };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+    }
+
+    {
+        couchbase::operations::lookup_in_request req{ id };
+        req.specs.add_spec(couchbase::protocol::subdoc_opcode::get, true, expiry_path);
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(0 == std::stoul(resp.fields[0].value));
     }
 }
