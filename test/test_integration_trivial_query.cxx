2e7fb4ea3afe59a2b1724c5ffaadc71357986aba
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2021-11-14 01:07:05 +0000

    SDKQE-2487: Port N1QL query tests from lcb

--- test/test_integration_trivial_query.cxx
+++ test/test_integration_trivial_query.cxx
@@ -16,12 +16,12 @@
  */
 
 #include "test_helper_integration.hxx"
+#include <couchbase/operations/management/collections.hxx>
+#include <couchbase/operations/management/query.hxx>
 
 TEST_CASE("integration: trivial non-data query", "[integration]")
 {
     test::utils::integration_test_guard integration;
-
-    test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
 
     if (!integration.ctx.version.supports_gcccp()) {
         test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
@@ -80,5 +80,148 @@ TEST_CASE("integration: query with handler capturing non-copyable object", "[int
         auto resp = f.get();
         INFO(resp.ctx.ec.message())
         REQUIRE_FALSE(resp.ctx.ec);
+    }
+}
+
+TEST_CASE("integration: query on a collection", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+    if (!integration.ctx.version.supports_collections()) {
+        return;
+    }
+    test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+
+    auto scope_name = test::utils::uniq_id("scope");
+    auto collection_name = test::utils::uniq_id("collection");
+    auto index_name = test::utils::uniq_id("index");
+    auto key = test::utils::uniq_id("foo");
+    tao::json::value value = {
+        { "a", 1.0 },
+        { "b", 2.0 },
+    };
+    auto json = couchbase::utils::json::generate(value);
+
+    uint64_t scope_uid;
+    uint64_t collection_uid;
+
+    {
+        couchbase::operations::management::scope_create_request req{ integration.ctx.bucket, scope_name };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        scope_uid = resp.uid;
+    }
+
+    {
+        couchbase::operations::management::collection_create_request req{ integration.ctx.bucket, scope_name, collection_name };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        collection_uid = resp.uid;
+    }
+
+    // wait until scope and collection have propagated
+    test::utils::wait_until([&]() {
+        couchbase::document_id id{ integration.ctx.bucket, "_default", "_default", "" };
+        couchbase::operations::management::collections_manifest_get_request req{ id };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        return resp.manifest.uid >= scope_uid && resp.manifest.uid >= collection_uid;
+    });
+
+    {
+        couchbase::operations::management::query_index_create_request req{};
+        req.bucket_name = integration.ctx.bucket;
+        req.scope_name = scope_name;
+        req.collection_name = collection_name;
+        req.index_name = index_name;
+        req.is_primary = true;
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+    }
+
+    couchbase::mutation_token mutation_token;
+
+    {
+        couchbase::document_id id{ integration.ctx.bucket, scope_name, collection_name, key };
+        couchbase::operations::insert_request req{ id, json };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        mutation_token = resp.token;
+    }
+
+    SECTION("correct scope and collection")
+    {
+        couchbase::operations::query_request req{ "SELECT a, b FROM " + collection_name + " WHERE META().id = \"" + key + "\"" };
+        req.bucket_name = integration.ctx.bucket;
+        req.scope_name = scope_name;
+        req.mutation_state = { mutation_token };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        REQUIRE(resp.payload.rows.size() == 1);
+        REQUIRE(value == couchbase::utils::json::parse(resp.payload.rows[0]));
+    }
+
+    SECTION("missing scope")
+    {
+        couchbase::operations::query_request req{ "SELECT a, b FROM " + collection_name + " WHERE META().id = \"" + key + "\"" };
+        req.bucket_name = integration.ctx.bucket;
+        req.scope_name = "missing_scope";
+        req.mutation_state = { mutation_token };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(resp.ctx.ec == couchbase::error::query_errc::index_failure);
+    }
+
+    SECTION("missing collection")
+    {
+        couchbase::operations::query_request req{ "SELECT a, b FROM missing_collection WHERE META().id = \"" + key + "\"" };
+        req.bucket_name = integration.ctx.bucket;
+        req.scope_name = scope_name;
+        req.mutation_state = { mutation_token };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE(resp.ctx.ec == couchbase::error::query_errc::index_failure);
+    }
+
+    SECTION("prepared")
+    {
+        couchbase::operations::query_request req{ "SELECT a, b FROM " + collection_name + " WHERE META().id = \"" + key + "\"" };
+        req.bucket_name = integration.ctx.bucket;
+        req.scope_name = scope_name;
+        req.mutation_state = { mutation_token };
+        req.adhoc = false;
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        REQUIRE(resp.payload.rows.size() == 1);
+        REQUIRE(value == couchbase::utils::json::parse(resp.payload.rows[0]));
+    }
+}
+
+TEST_CASE("integration: read only with no results", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+
+    if (!integration.ctx.version.supports_gcccp()) {
+        test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+    }
+
+    {
+        couchbase::operations::query_request req{ "SELECT * FROM " + integration.ctx.bucket + " LIMIT 0" };
+        auto resp = test::utils::execute(integration.cluster, req);
+        REQUIRE_FALSE(resp.ctx.ec);
+        REQUIRE(resp.payload.rows.empty());
+    }
+}
+
+TEST_CASE("integration: invalid query", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+
+    if (!integration.ctx.version.supports_gcccp()) {
+        test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+    }
+
+    {
+        couchbase::operations::query_request req{ "I'm not n1ql" };
+        auto resp = test::utils::execute(integration.cluster, req);
+        INFO(resp.ctx.ec.message());
+        REQUIRE(resp.ctx.ec == couchbase::error::common_errc::parsing_failure);
     }
 }
