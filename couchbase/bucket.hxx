283f69beddc8cae708e6ffaf78ca520801c053bc
Sergey Avseyev <sergey.avseyev@gmail.com>
2022-01-26 23:29:10 +0300

    CXXCBC-92: include CAS into key_value error context

--- couchbase/bucket.hxx
+++ couchbase/bucket.hxx
@@ -304,6 +304,7 @@ class bucket : public std::enable_shared_from_this<bucket>
             auto resp = msg ? encoded_response_type(std::move(*msg)) : encoded_response_type{};
             error_context::key_value ctx{ cmd->request.id };
             ctx.opaque = resp.opaque();
+            ctx.cas = resp.cas();
             ctx.ec = ec;
             if (ctx.ec && ctx.opaque == 0) {
                 ctx.opaque = cmd->request.opaque;
