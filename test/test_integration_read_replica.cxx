9d9c12ec28408db3319627a36f02f931e6e1d65d
Sergey Avseyev <sergey.avseyev@gmail.com>
2022-07-26 19:30:01 +0300

    CXXCBC-52: fix read replica behavior for missing keys
    It also exposes flags as @internal API on the result (it might be
    removed later) and updates docs with code snippet of how to use the
    custom transcoder

--- test/test_integration_read_replica.cxx
+++ test/test_integration_read_replica.cxx
@@ -71,7 +71,7 @@ TEST_CASE("integration: get all replicas", "[integration]")
 
     std::string scope_name{ "_default" };
     std::string collection_name{ "_default" };
-    std::string key = test::utils::uniq_id("get_any_replica");
+    std::string key = test::utils::uniq_id("get_all_replica");
 
     {
         couchbase::core::document_id id{ integration.ctx.bucket, scope_name, collection_name, key };
@@ -90,4 +90,74 @@ TEST_CASE("integration: get all replicas", "[integration]")
         auto responses_from_active = std::count_if(result.begin(), result.end(), [](const auto& r) { return !r.is_replica(); });
         REQUIRE(responses_from_active == 1);
     }
+}
+
+TEST_CASE("integration: get all replicas with missing key", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+
+    if (integration.number_of_nodes() <= integration.number_of_replicas()) {
+        return;
+    }
+
+    test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+
+    std::string scope_name{ "_default" };
+    std::string collection_name{ "_default" };
+    std::string key = test::utils::uniq_id("get_all_replica_missing_key");
+
+    {
+        auto collection =
+          couchbase::cluster(integration.cluster).bucket(integration.ctx.bucket).scope(scope_name).collection(collection_name);
+        auto [ctx, result] = collection.get_all_replicas(key, {}).get();
+        REQUIRE(ctx.ec() == couchbase::errc::key_value::document_not_found);
+        REQUIRE(result.empty());
+    }
+}
+
+TEST_CASE("integration: get any replica with missing key", "[integration]")
+{
+    test::utils::integration_test_guard integration;
+
+    if (integration.number_of_nodes() <= integration.number_of_replicas()) {
+        return;
+    }
+
+    test::utils::open_bucket(integration.cluster, integration.ctx.bucket);
+
+    std::string scope_name{ "_default" };
+    std::string collection_name{ "_default" };
+    std::string key = test::utils::uniq_id("get_any_replica_missing_key");
+
+    {
+        auto collection =
+          couchbase::cluster(integration.cluster).bucket(integration.ctx.bucket).scope(scope_name).collection(collection_name);
+        auto [ctx, result] = collection.get_any_replica(key, {}).get();
+        REQUIRE(ctx.ec() == couchbase::errc::key_value::document_irretrievable);
+    }
+}
+
+TEST_CASE("unit: get any replica result with custom transcoder", "[integration]")
+{
+    couchbase::get_replica_result result{
+        couchbase::cas{ 0 }, true, { std::byte{ 0xde }, std::byte{ 0xad }, std::byte{ 0xbe }, std::byte{ 0xaf } }, 0xcafebebe
+    };
+
+    // clang-format off
+    //! [smuggling-transcoder]
+struct smuggling_transcoder {
+    using value_type = std::pair<std::vector<std::byte>, std::uint32_t>;
+
+    static auto decode(const std::vector<std::byte>& data, std::uint32_t flags) -> value_type
+    {
+        return { data, flags };
+    }
+};
+
+auto [data, flags] = result.content_as<smuggling_transcoder>();
+    //! [smuggling-transcoder]
+    // clang-format on
+
+    REQUIRE(flags == 0xcafebebe);
+    REQUIRE(data == std::vector{ std::byte{ 0xde }, std::byte{ 0xad }, std::byte{ 0xbe }, std::byte{ 0xaf } });
 }
