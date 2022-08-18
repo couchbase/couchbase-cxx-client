cfaa302788c0c9334bb6331fa168ffb949b921c6
Sergey Avseyev <sergey.avseyev@gmail.com>
2022-08-18 19:45:30 +0300

    add quotes to mutate_in macros

--- core/impl/subdoc/mutate_in_macro.cxx
+++ core/impl/subdoc/mutate_in_macro.cxx
@@ -23,13 +23,13 @@ namespace couchbase::subdoc
 auto
 to_mutate_in_macro(std::string_view input) -> std::optional<couchbase::subdoc::mutate_in_macro>
 {
-    if (input == "${Mutation.CAS}") {
+    if (input == "\"${Mutation.CAS}\"") {
         return couchbase::subdoc::mutate_in_macro::cas;
     }
-    if (input == "${Mutation.seqno}") {
+    if (input == "\"${Mutation.seqno}\"") {
         return couchbase::subdoc::mutate_in_macro::sequence_number;
     }
-    if (input == "${Mutation.value_crc32c}") {
+    if (input == "\"${Mutation.value_crc32c}\"") {
         return couchbase::subdoc::mutate_in_macro::value_crc32c;
     }
     return {};
@@ -38,26 +38,26 @@ to_mutate_in_macro(std::string_view input) -> std::optional<couchbase::subdoc::m
 auto
 to_binary(couchbase::subdoc::mutate_in_macro value) -> std::vector<std::byte>
 {
-    // echo -n '${Mutation.CAS}' | ruby -e 'puts ARGF.read.chars.map{|c| format("std::byte{0x%02x}", c.ord)}.join(", ")'
+    // echo -n '"${Mutation.CAS}"' | ruby -e 'puts ARGF.read.chars.map{|c| format("std::byte{0x%02x}", c.ord)}.join(", ")'
     static std::vector cas_bytes{
-        std::byte{ 0x24 }, std::byte{ 0x7b }, std::byte{ 0x4d }, std::byte{ 0x75 }, std::byte{ 0x74 },
-        std::byte{ 0x61 }, std::byte{ 0x74 }, std::byte{ 0x69 }, std::byte{ 0x6f }, std::byte{ 0x6e },
-        std::byte{ 0x2e }, std::byte{ 0x43 }, std::byte{ 0x41 }, std::byte{ 0x53 }, std::byte{ 0x7d },
+        std::byte{ 0x22 }, std::byte{ 0x24 }, std::byte{ 0x7b }, std::byte{ 0x4d }, std::byte{ 0x75 }, std::byte{ 0x74 },
+        std::byte{ 0x61 }, std::byte{ 0x74 }, std::byte{ 0x69 }, std::byte{ 0x6f }, std::byte{ 0x6e }, std::byte{ 0x2e },
+        std::byte{ 0x43 }, std::byte{ 0x41 }, std::byte{ 0x53 }, std::byte{ 0x7d }, std::byte{ 0x22 },
     };
 
-    // echo -n '${Mutation.seqno}' | ruby -e 'puts ARGF.read.chars.map{|c| format("std::byte{0x%02x}", c.ord)}.join(", ")'
+    // echo -n '"${Mutation.seqno}"' | ruby -e 'puts ARGF.read.chars.map{|c| format("std::byte{0x%02x}", c.ord)}.join(", ")'
     static std::vector seqno_bytes{
-        std::byte{ 0x24 }, std::byte{ 0x7b }, std::byte{ 0x4d }, std::byte{ 0x75 }, std::byte{ 0x74 }, std::byte{ 0x61 },
-        std::byte{ 0x74 }, std::byte{ 0x69 }, std::byte{ 0x6f }, std::byte{ 0x6e }, std::byte{ 0x2e }, std::byte{ 0x73 },
-        std::byte{ 0x65 }, std::byte{ 0x71 }, std::byte{ 0x6e }, std::byte{ 0x6f }, std::byte{ 0x7d },
+        std::byte{ 0x22 }, std::byte{ 0x24 }, std::byte{ 0x7b }, std::byte{ 0x4d }, std::byte{ 0x75 }, std::byte{ 0x74 }, std::byte{ 0x61 },
+        std::byte{ 0x74 }, std::byte{ 0x69 }, std::byte{ 0x6f }, std::byte{ 0x6e }, std::byte{ 0x2e }, std::byte{ 0x73 }, std::byte{ 0x65 },
+        std::byte{ 0x71 }, std::byte{ 0x6e }, std::byte{ 0x6f }, std::byte{ 0x7d }, std::byte{ 0x22 },
     };
 
-    // echo -n '${Mutation.value_crc32c}' | ruby -e 'puts ARGF.read.chars.map{|c| format("std::byte{0x%02x}", c.ord)}.join(", ")'
+    // echo -n '"${Mutation.value_crc32c}"' | ruby -e 'puts ARGF.read.chars.map{|c| format("std::byte{0x%02x}", c.ord)}.join(", ")'
     static std::vector crc32_bytes{
-        std::byte{ 0x24 }, std::byte{ 0x7b }, std::byte{ 0x4d }, std::byte{ 0x75 }, std::byte{ 0x74 }, std::byte{ 0x61 },
-        std::byte{ 0x74 }, std::byte{ 0x69 }, std::byte{ 0x6f }, std::byte{ 0x6e }, std::byte{ 0x2e }, std::byte{ 0x76 },
-        std::byte{ 0x61 }, std::byte{ 0x6c }, std::byte{ 0x75 }, std::byte{ 0x65 }, std::byte{ 0x5f }, std::byte{ 0x63 },
-        std::byte{ 0x72 }, std::byte{ 0x63 }, std::byte{ 0x33 }, std::byte{ 0x32 }, std::byte{ 0x63 }, std::byte{ 0x7d },
+        std::byte{ 0x22 }, std::byte{ 0x24 }, std::byte{ 0x7b }, std::byte{ 0x4d }, std::byte{ 0x75 }, std::byte{ 0x74 }, std::byte{ 0x61 },
+        std::byte{ 0x74 }, std::byte{ 0x69 }, std::byte{ 0x6f }, std::byte{ 0x6e }, std::byte{ 0x2e }, std::byte{ 0x76 }, std::byte{ 0x61 },
+        std::byte{ 0x6c }, std::byte{ 0x75 }, std::byte{ 0x65 }, std::byte{ 0x5f }, std::byte{ 0x63 }, std::byte{ 0x72 }, std::byte{ 0x63 },
+        std::byte{ 0x33 }, std::byte{ 0x32 }, std::byte{ 0x63 }, std::byte{ 0x7d }, std::byte{ 0x22 },
     };
 
     switch (value) {
