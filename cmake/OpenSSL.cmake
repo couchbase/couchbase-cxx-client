a2667b0a9e5cba438e92659713bba663868d0897
Sergey Avseyev <sergey.avseyev@gmail.com>
2022-09-22 12:46:18 +0300

    improve OpenSSL detection on Windows

--- cmake/OpenSSL.cmake
+++ cmake/OpenSSL.cmake
@@ -1,5 +1,6 @@
 option(COUCHBASE_CXX_CLIENT_POST_LINKED_OPENSSL "Rely on application to link OpenSSL library" FALSE)
 option(COUCHBASE_CXX_CLIENT_USE_HOMEBREW_TO_DETECT_OPENSSL "Use homebrew to determine OpenSSL root directory" TRUE)
+option(COUCHBASE_CXX_CLIENT_USE_SCOOP_TO_DETECT_OPENSSL "Use scoop to determine OpenSSL root directory" TRUE)
 
 if(COUCHBASE_CXX_CLIENT_POST_LINKED_OPENSSL)
   message(
@@ -10,12 +11,26 @@ else()
   if(COUCHBASE_CXX_CLIENT_STATIC_OPENSSL)
     set(OPENSSL_USE_STATIC_LIBS ON)
   endif()
-  if(APPLE AND COUCHBASE_CXX_CLIENT_USE_HOMEBREW_TO_DETECT_OPENSSL)
-    execute_process(
-      COMMAND brew --prefix openssl@1.1
-      OUTPUT_VARIABLE OPENSSL_ROOT_DIR
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
-    message(STATUS "Found OpenSSL prefix using homebrew: ${OPENSSL_ROOT_DIR}")
+  if(NOT OPENSSL_ROOT_DIR)
+    if(APPLE AND COUCHBASE_CXX_CLIENT_USE_HOMEBREW_TO_DETECT_OPENSSL)
+      execute_process(
+        COMMAND brew --prefix openssl@1.1
+        OUTPUT_VARIABLE OPENSSL_ROOT_DIR
+        ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+      if(OPENSSL_ROOT_DIR)
+        message(STATUS "Found OpenSSL prefix using homebrew: ${OPENSSL_ROOT_DIR}")
+      endif()
+    endif()
+    if(MSVC AND COUCHBASE_CXX_CLIENT_USE_SCOOP_TO_DETECT_OPENSSL)
+      find_program(POWERSHELL powershell)
+      execute_process(
+        COMMAND ${POWERSHELL} scoop prefix openssl1
+        OUTPUT_VARIABLE OPENSSL_ROOT_DIR
+        ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+      if(OPENSSL_ROOT_DIR)
+        message(STATUS "Found OpenSSL prefix using scoop: ${OPENSSL_ROOT_DIR}")
+      endif()
+    endif()
   endif()
   include(FindOpenSSL)
   if(OPENSSL_LIBRARIES AND OPENSSL_INCLUDE_DIR)
