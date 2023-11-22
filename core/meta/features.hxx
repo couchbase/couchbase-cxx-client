24d7b7f935cedbff3655138d8b9711d7ab53de3f
Dimitris Christodoulou <dimitris.christodoulou@couchbase.com>
2023-11-22 15:54:36 +0000

    Add feature flag

--- core/meta/features.hxx
+++ core/meta/features.hxx
@@ -78,3 +78,9 @@
  * core cluster implementation has been hidden and not accessible through the public API
  */
 #define COUCHBASE_CXX_CLIENT_HAS_CORE_CLUSTER_HIDDEN 1
+
+/**
+ * expiration_time has been renamed to timeout in transactions_options and transactions_config
+ * kv_timeout removed from transactions_options and transactions_config
+ */
+#define COUCHBASE_CXX_CLIENT_TRANSACTIONS_OPTIONS_HAVE_TIMEOUT 1
