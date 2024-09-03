f10ee65e0a2a8ad10347d7257ec2ceaf4f02c9cb
Sergey Avseyev <sergey.avseyev@gmail.com>
2024-09-03 13:46:07 -0700

    remove incorrect snippet for get_all_replicas with callback

--- couchbase/collection.hxx
+++ couchbase/collection.hxx
@@ -375,13 +375,6 @@ public:
    * @note Individual errors are ignored, so you can think of this API as a best effort
    * approach which explicitly emphasises availability over consistency.
    *
-   * Select preferred server group in connection options:
-   * @snippet{trimleft} test_integration_read_replica.cxx select-preferred_server_group
-   *
-   * Restrict get_any_replica to reach nodes that belong to selected server
-   * group only:
-   * @snippet{trimleft} test_integration_read_replica.cxx get_any_replica-with-read_preference
-   *
    * @param document_id the document id which is used to uniquely identify it.
    * @param options the custom options
    * @param handler the handler that implements @ref get_all_replicas_handler
