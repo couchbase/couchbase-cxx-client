e6ec2447c611cf7b522d152c141f83a1ddff10d6
Sergey Avseyev <sergey.avseyev@gmail.com>
2024-10-28 21:22:23 -0700

    WIP: OpenTelemetry integration refactoring

---
+++ core/metrics/otel_meter.hxx
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *   Copyright 2021 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#pragma once
+
+#include <couchbase/metrics/meter.hxx>
+
+namespace couchbase::core::metrics
+{
+
+class otel_meter_impl;
+
+class otel_meter : public couchbase::metrics::meter
+{
+public:
+  auto get_value_recorder(const std::string& name, const std::map<std::string, std::string>& tags)
+    -> std::shared_ptr<couchbase::metrics::value_recorder> override;
+
+private:
+  std::unique_ptr<otel_meter_impl> impl_;
+};
+} // namespace couchbase::core::metrics
