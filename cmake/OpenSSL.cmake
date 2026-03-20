d8eb960cf1351b714f7fe6b0d694a9d9f8ae1669
Jared Casey <thejcfactor@users.noreply.github.com>
2026-03-20 11:02:49 -0500

    Update regex for parsing curl.se Mozilla certs header date (#908)
    Changes
    -------
    * Update regex for parsing curl.se Mozilla certs header date
    * Output warning if certs header date not found

--- cmake/OpenSSL.cmake
+++ cmake/OpenSSL.cmake
@@ -209,10 +209,18 @@ if(COUCHBASE_CXX_CLIENT_EMBED_MOZILLA_CA_BUNDLE)
   file(READ "${COUCHBASE_CXX_CLIENT_EMBED_MOZILLA_CA_BUNDLE_ROOT}/mozilla-ca-bundle.crt" CA_BUNDLE_CONTENT)
   string(
     REGEX MATCH
-          "Certificate data from Mozilla as of: ([^\n]*)"
+          "Certificate data from Mozilla (as of|last updated on): ([^\n]*)"
           CA_BUNDLE_DATE_LINE
           ${CA_BUNDLE_CONTENT})
-  set(COUCHBASE_CXX_CLIENT_MOZILLA_CA_BUNDLE_DATE "${CMAKE_MATCH_1}")
+  if(CA_BUNDLE_DATE_LINE)
+    set(COUCHBASE_CXX_CLIENT_MOZILLA_CA_BUNDLE_DATE "${CMAKE_MATCH_2}")
+  else()
+    message(
+      WARNING
+        "Failed to extract Mozilla CA bundle date from certificate file; "
+        "COUCHBASE_CXX_CLIENT_MOZILLA_CA_BUNDLE_DATE will be set to 'unknown'.")
+    set(COUCHBASE_CXX_CLIENT_MOZILLA_CA_BUNDLE_DATE "unknown")
+  endif()
 else()
   set(CA_BUNDLE_CONTENT "")
 endif()
